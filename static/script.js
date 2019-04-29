3b538d9af6e8bf6bb59b27e02b92277d52f7211e
Ahnaf Hasan <ahasan1@stuy.edu>
2019-04-29 11:47:21 -0400

    add comments

--- static/script.js
+++ static/script.js
@@ -3,41 +3,49 @@ var right = document.getElementById('r_arr')
 /** @type {HTMLImageElement} */
 var image = document.getElementById('image')
 
+// Current active navbar option
 var active = 0
+
+/**
+* Function to change slides to the right. Originally made to fade in and out.
+*/
 function fade_right() {
     console.log('clicked')
-    var num = Number(image.src.substr(image.src.indexOf('(') + 1, image.src.indexOf(')') - image.src.indexOf('(') - 1))
+    var num = Number(image.src.substr(image.src.indexOf('(') + 1, image.src.indexOf(')') - image.src.indexOf('(') - 1)) // the number of the slide
     var format = 'jpg'
-    if (num >= 18) {
+    if (num >= 18) { // the final slide
         image.src = `/static/Node.js (19).jpg`
-        right.style.opacity = 0.5
+        right.style.opacity = 0.5 // make right arrow dimmer to show disabled arrow
         active = 8
-        change_active()
+        change_active() // change current active navbar
         return
     }
     num += 1
-    if (num == 7 || num == 8 || num == 11) {
+    if (num == 7 || num == 8 || num == 11) { // the gifs
         format = 'gif'
     }else{
         format = 'jpg'
     }
     left.style.opacity = 1
-    image.src = `/static/Node.js (${num}).${format}`
-    if (num == 0 || num == 3 || num == 4 || num == 6 || num == 10 || num == 13 || num == 16 || num == 18) {
+    image.src = `/static/Node.js (${num}).${format}` // change slide image
+    if (num == 0 || num == 3 || num == 4 || num == 6 || num == 10 || num == 13 || num == 16 || num == 18) { // top nav sections
       active += 1
     }
-    change_active()
-    change()
+    change_active() // change current active navbar
+    change() // set image size
 }
 
+/**
+* Function to change slides to the left. Originally made to fade in and out.
+*/
 function fade_left() {
-    var num = Number(image.src.substr(image.src.indexOf('(') + 1, image.src.indexOf(')') - image.src.indexOf('(') - 1))
+    var num = Number(image.src.substr(image.src.indexOf('(') + 1, image.src.indexOf(')') - image.src.indexOf('(') - 1)) // number of slide
     var format = 'jpg'
     if (num <= 1) {
         image.src = `/static/Node.js (0).jpg`
-        left.style.opacity = 0.5
+        left.style.opacity = 0.5 // dim arrow to show disable arrow
         active = 0
-        change_active()
+        change_active() // change current active navbar
         return
     }
     num -= 1
@@ -47,14 +55,17 @@ function fade_left() {
         format = 'jpg'
     }
     right.style.opacity = 1
-    image.src = `/static/Node.js (${num}).${format}`
-    if (num == 3 || num == 4 || num == 6 || num == 10 || num == 13 || num == 16 || num == 18 || num == 19) {
+    image.src = `/static/Node.js (${num}).${format}` // change slide image
+    if (num == 3 || num == 4 || num == 6 || num == 10 || num == 13 || num == 16 || num == 18 || num == 19) { // top nav sections
       active -= 1
     }
-    change_active()
+    change_active() //change current active navbar
     change()
 }
 
+/**
+* Function to change image sizes
+*/
 function change() {
     var sizes = {
         x: window.innerWidth / 2,
@@ -64,17 +75,23 @@ function change() {
     image.height = sizes.y + 360
 }
 
+/**
+* Function to change the current active navbar
+*/
 function change_active() {
-  var links = document.getElementsByClassName('topnav')[0].children
+  var links = document.getElementsByClassName('topnav')[0].children // the links (sections)
   for (i = 0; i < links.length; i++) {
     if (i == active) {
-      links[i].setAttribute('class', 'active')
+      links[i].setAttribute('class', 'active') // add class active
     }else{
-      links[i].setAttribute('class', '')
+      links[i].setAttribute('class', '') // remove class active
     }
   }
 }
 
+/**
+* Function to jump to given slide
+*/
 function jump_to(jump) {
     var starter = `/static/Node.js (`
     left.style.opacity = 1
@@ -117,7 +134,7 @@ function jump_to(jump) {
         right.style.opacity = 0.5
         active = 8
     }
-    change_active()
+    change_active() // change current active navbar
 }
 
 /**
@@ -134,7 +151,7 @@ function wheel_change(e) {
     return
 }
 
-change()
+change() // initial change to set height and width
 
 right.addEventListener('click', fade_right )
 left.addEventListener('click', fade_left )
